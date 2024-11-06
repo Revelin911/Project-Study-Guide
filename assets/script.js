e8fc7a51fbeb58f547c5c47a8bc7c9de859cb09e
Revelin911 <revelin911@mail.com>
2024-11-05 17:36:45 -0700

    added JS console vars

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
-const topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
+// const topics = "HTML";
+// if (topics === "HTML") {
+//   console.log("Let's study HTML!");
+// } else if (topics === "CSS") {
+// console.log("Let's study CSS!");
+// } else if (topic === "Git") {
+//   console.log("Let's study Git!");
+// } else if (topic === "Javascript") {
+//     console.log("Let's study Javascript!");
+// } else {
+//     console.log("Please try again!");
+// }
+
+// const shapes = ["triangle", "square", "pentagon", "circle"];
+// for(let x=0; x<shapes.length; x++) {
+//   console.log(shapes[x]);
+// }
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+for (let x = 0; x < topics.length; x++) {
+  console.log(topics[x]);
+}
