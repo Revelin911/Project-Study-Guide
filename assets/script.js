ca5dccdf98f8d05f6acc74ab3099dccdf3b95d7b
Revelin911 <revelin911@mail.com>
2024-11-12 12:58:45 -0700

    fixed bug in prework

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,32 @@
-
 const topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+const randomTopic = topics[Math.floor(Math.random() * topics.lenth)];
+
+
 function listTopics() {
   for (let x = 0; x < topics.length; x++) {
     console.log(topics[x]);
   }
 }
+
 function selectTopic() {
   // const topics = "HTML";
-  if (topics === 'HTML') {
+  if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-  } else if (topics === 'CSS') {
+  } else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-  } else if (topics === 'Git') {
+  } else if (randomTopic === 'Git') {
     console.log("Let's study Git!");
-  } else if (topics === 'Javascript') {
+  } else if (randomTopic === 'Javascript') {
     console.log("Let's study Javascript!");
   } else {
     console.log('Please try again!');
   }
 }
+console.log("Here are the topics we learned through Prework:");
 listTopics()
+console.log("Which topic should we study first?");
+selectTopic();
 
 //   const shapes = ["triangle", "square", "pentagon", "circle"];
 //   for (let x = 0; x < shapes.length; x++) {
